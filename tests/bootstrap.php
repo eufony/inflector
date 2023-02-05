1af9a477e289eaa048504ccb0e7506ae16f41ef4
Alpin H. Gencer <ah@gencer.us>
2023-02-05 17:26:56 +0100

    Refresh repository

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,5 +3,5 @@
 require __DIR__ . "/../vendor/autoload.php";
 
 echo "\n";
-echo "Testsuite for the Eufony inflector package by Alpin Gencer and contributors.\n";
+echo "Testsuite for the Eufony inflector package by Alpin H. Gencer and contributors.\n";
 echo "\n";
